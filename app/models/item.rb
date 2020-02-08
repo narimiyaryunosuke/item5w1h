d4061cf67f5645e50d0465243297d918f9f5a125
EC2 Default User <ec2-user@ip-172-31-93-118.ec2.internal>
2020-02-08 09:28:14 +0000

    add  price validate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   validates :code, presence: true
   validates :name, presence: true
+  validates :price, presence: true
   validates :url, presence: true
   validates :image_url, presence: true
+  
+  
 end
