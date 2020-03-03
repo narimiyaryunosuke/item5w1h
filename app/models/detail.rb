99d1091ce4c51a33cd74d1a69601154e464cf430
EC2 Default User <ec2-user@ip-172-31-93-118.ec2.internal>
2020-03-03 06:10:38 +0000

    add validation

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -2,10 +2,10 @@ class Detail < ApplicationRecord
   belongs_to :favorite
   
   validates :name, length: { maximum: 100 }
-  validates :purchase_date, length: { maximum: 50 }
+  validates :purchase_date, length: { maximum: 100 }
   validates :place, length: { maximum: 100 }
   validates :person, length: { maximum: 100 }
-  validates :reason, length: { maximum: 100 }
+  validates :reason, length: { maximum: 300 }
   validates :way, length: { maximum: 100 }
   
 end
